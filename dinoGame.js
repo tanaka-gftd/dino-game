ee948041a01c2a394e272606aa4ae22207d39393
Tanaka <f.tanaka@gftd.works>
2023-01-10 13:11:52 +0900

    敵キャラとして、サボテンと鳥を表示させるようにした

--- dinoGame.js
+++ dinoGame.js
@@ -84,16 +84,30 @@ function init(){
 //パラパラ漫画のようにするため、本関数は何度も呼び出される
 function ticker(){
 
-  //パラパラ漫画のようにするため、一旦画面の中身をクリア
+  //パラパラ漫画のようにするため、本関数実行の度に一旦画面の中身をクリア
   ctx.clearRect(0,0,canvas.width,canvas.height);
 
-  // TODO 敵キャラクタの生成
+  //敵サボテンはランダムなタイミングで生成
+  if(Math.floor(Math.random() * 100) === 0){
+    createCactus();
+  };
+
+  //敵バードはランダムなタイミングで生成
+  if(Math.floor(Math.random() * 100) === 0){
+    createBird();
+  };
 
   //恐竜の移動
   moveDino();
 
+  //敵の移動
+  moveEnemies();
+
   //恐竜の描画
   drawDino();
+
+  //敵キャラクタの描画
+  drawEnemies();
 
   // TODO 当たり判定処理
 
@@ -113,6 +127,35 @@ function createDino(){
     height: game.image.dino.height,  //恐竜の画像の縦幅の数値
     image: game.image.dino  //恐竜の画像オブジェクト
   }
+};
+
+
+//敵サボテンの作成
+//敵サボテンは画面下段で出現し、右端から左方向に移動する
+function createCactus(){
+  game.enemies.push({
+    x: canvas.width + game.image.cactus.width / 2,  //初期位置、すなわち出現位置は画面右端
+    y: canvas.height - game.image.cactus.height / 2,  //画面下端
+    width: game.image.cactus.width,
+    height: game.image.cactus.height,
+    moveX: -10,  //左方向に移動
+    image: game.image.cactus
+  });
+};
+
+
+//敵バードの生成
+//敵バードはランダムな高さで出現し、右端から左方向に移動する
+function createBird(){
+  const birdY = Math.random() * (300 - game.image.bird.height) + 150;  //ランダムな高さを指定
+  game.enemies.push({
+    x: canvas.width + game.image.bird.width / 2,  //初期位置、すなわち出現位置は画面右端
+    y: birdY,
+    width: game.image.bird.width,
+    height: game.image.bird.height,
+    moveX: -15,  //左方向に移動
+    image: game.image.bird
+  });
 };
 
 
@@ -152,6 +195,21 @@ function moveDino(){
 };
 
 
+//敵キャラの移動、画面の外に出た敵キャラは削除
+function moveEnemies(){
+
+  //敵キャラの移動
+  for(const enemy of game.enemies){
+    enemy.x += enemy.moveX;
+  };
+
+  //画面の外に出た敵キャラを、配列から削除
+  //ここでのxは、画面右端からの距離。
+  //敵キャラクタのx座標が画像の横幅分を超えて外にでたら削除したいので、まだ外にはみ出てない物だけを残す
+  game.enemies = game.enemies.filter(enemy => enemy.x > -enemy.width);
+};
+
+
 //恐竜の描画
 /* 
   ctx.drawImageで指定した座標は画像の左上になる事に注意。
@@ -164,7 +222,17 @@ function moveDino(){
 */
 function drawDino(){
   ctx.drawImage(game.image.dino, game.dino.x - game.dino.width / 2, game.dino.y - game.dino.height / 2);
-}
+};
+
+
+//各敵キャラを描画
+function drawEnemies(){
+
+  //敵キャラの情報が格納された配列enemiesから各要素を取り出して描画していく
+  for(const enemy of game.enemies){
+    ctx.drawImage(enemy.image, enemy.x - enemy.width / 2, enemy.y - enemy.height / 2);
+  };
+};
 
 
 //キー入力でジャンプするようにする
